c7fe7319572f2f6e7dd7f73205dd3cea96a89df7
mikhael dantas <mikhael-dcs@outlook.com>
2023-04-02 19:27:12 -0300

    abstracted all components and iimplemented layout

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,180 +1,50 @@
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4 p-4">
-    <div class="bg-red-500 h-[20rem]">
+    <div class="">
       <div class="h-[100%] w-full flex justify-center items-center">
         <img
           src="https://cf.workana.com/logos/c81d7b293b7428dbf6b29d52daf0abbc/rs/Designsemnome2_192_192.png"
           alt="Image 1"
           class="rounded-full h-40 w-40 object-cover object-center max-h-full max-w-full border-2 border-gray-300 shadow-sm"
         />
-        <i class="fas fa-icon text-gray-500 mx-4 text-2xl">X</i>
+        <i class="fas fa-icon text-gray-500 mx-4 text-2xl">↓↑</i>
         <img
-          src="https://i.ytimg.com/vi/DXUAyRRkI6k/mqdefault.jpg"
+          src="https://media.licdn.com/dms/image/D4E03AQGLay86biXNgQ/profile-displayphoto-shrink_200_200/0/1673301987295?e=1686182400&v=beta&t=wyb_9uXGtQD7hema2rtYRSAcgMhE-oNzo8mWJIw3iMk"
           alt="Image 2"
           class="rounded-full h-40 w-40 object-cover object-center max-h-full max-w-full border-2 border-gray-300 shadow-sm"
         />
       </div>
     </div>
-    <div class="bg-blue-500 p-4">
-      <form @submit.prevent="createArticle" class="space-y-4">
-        <div>
-          <label for="name" class="block text-sm font-medium text-gray-700"
-            >Name</label
-          >
-          <input
-            v-model="newArticle.name"
-            type="text"
-            id="name"
-            class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-          />
-        </div>
-        <div>
-          <label
-            for="description"
-            class="block text-sm font-medium text-gray-700"
-            >Description</label
-          >
-          <input
-            v-model="newArticle.description"
-            type="text"
-            id="description"
-            class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-          />
-        </div>
-        <div>
-          <label for="tags" class="block text-sm font-medium text-gray-700"
-            >Tags</label
-          >
-          <div class="relative mt-1">
-            <input
-              v-model="tagInput"
-              @keydown.space.prevent="addTag"
-              type="text"
-              id="tags"
-              class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-blue-500 focus:border-blue-500"
-            />
-            <div class="flex flex-wrap mt-2">
-              <span
-                v-for="(tag, index) in newArticle.tags"
-                :key="index"
-                class="bg-blue-500 text-white rounded-full px-3 py-1 mr-1 mb-1"
-              >
-                {{ tag }}
-                <button
-                  @click.prevent="removeTag(index)"
-                  class="ml-1 text-white"
-                >
-                  ×
-                </button>
-              </span>
-            </div>
-          </div>
-        </div>
-        <button
-          type="submit"
-          class="w-full py-2 px-4 bg-blue-600 text-white rounded-md hover:bg-blue-700"
-        >
-          Create Article
-        </button>
-      </form>
-    </div>
-    <div class="bg-green-500 p-4">
-      <ul>
-        <li
-          v-for="(article, index) in articles"
-          :key="index"
-          @click="selectArticle(index)"
-          class="py-2 px-4 bg-white rounded-md mb-2 cursor-pointer hover:bg-gray-200"
-        >
-          <h3 class="font-bold">{{ article.name }}</h3>
-          <p>{{ article.description }}</p>
-          <p>{{ article.tags }}</p>
-        </li>
-      </ul>
-    </div>
-    <div class="bg-yellow-500 p-4" v-if="selectedArticle !== null">
-      <form @submit.prevent="updateArticle" class="space-y-4">
-        <div>
-          <label for="edit-id" class="block text-sm font-medium text-gray-700"
-            >ID</label
-          >
-          <input
-            v-model="editedArticle.id"
-            type="text"
-            id="edit-id"
-            readonly
-            class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-yellow-500 focus:border-yellow-500"
-          />
-        </div>
-        <div>
-          <label for="edit-name" class="block text-sm font-medium text-gray-700"
-            >Name</label
-          >
-          <input
-            v-model="editedArticle.name"
-            type="text"
-            id="edit-name"
-            class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-yellow-500 focus:border-yellow-500"
-          />
-        </div>
-        <div>
-          <label
-            for="edit-description"
-            class="block text-sm font-medium text-gray-700"
-            >Description</label
-          >
-          <input
-            v-model="editedArticle.description"
-            type="text"
-            id="edit-description"
-            class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-yellow-500 focus:border-yellow-500"
-          />
-        </div>
-        <div>
-          <label for="edit-tags" class="block text-sm font-medium text-gray-700"
-            >Tags</label
-          >
-          <div class="relative mt-1">
-            <input
-              v-model="editTagInput"
-              @keydown.space.prevent="addEditTag"
-              type="text"
-              id="edit-tags"
-              class="mt-1 block w-full py-2 px-3 border border-gray-300 bg-white rounded-md shadow-sm focus:outline-none focus:ring-yellow-500 focus:border-yellow-500"
-            />
-            <div class="flex flex-wrap mt-2">
-              <span
-                v-for="(tag, index) in editedArticle.tags"
-                :key="index"
-                class="bg-yellow-500 text-white rounded-full px-3 py-1 mr-1 mb-1"
-              >
-                {{ tag }}
-                <button
-                  @click.prevent="removeEditTag(index)"
-                  class="ml-1 text-white"
-                >
-                  ×
-                </button>
-              </span>
-            </div>
-          </div>
-        </div>
-        <button
-          type="submit"
-          class="w-full py-2 px-4 bg-yellow-600 text-white rounded-md hover:bg-yellow-700"
-        >
-          Update Article
-        </button>
-      </form>
-    </div>
+    <CreateArticleForm
+      :new-article="newArticle"
+      @addTag="addTag"
+      @removeTag="removeTag"
+      @submit="createArticle"
+    />
+    <ArticleList
+      :articles="articles"
+      :selected-article="selectedArticle"
+      @selectArticle="selectArticle"
+    />
+    <EditArticleForm
+      :edited-article="editedArticle"
+      :selected-article="selectedArticle"
+      @addEditTag="addEditTag"
+      @removeEditTag="removeEditTag"
+      @updateArticle="updateArticle"
+      @deleteArticle="deleteArticle"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, ref } from "vue";
 import axios from "axios";
+import CreateArticleForm from "../components/CreateArticleForm.vue";
+import ArticleList from "../components/ArticleList.vue";
+import EditArticleForm from "../components/EditArticleForm.vue";
 
-interface Article {
+export interface Article {
   id: string;
   name: string;
   description: string;
@@ -183,10 +53,14 @@ interface Article {
 
 export default defineComponent({
   name: "HomeView",
-  components: {},
+  components: {
+    CreateArticleForm,
+    ArticleList,
+    EditArticleForm,
+  },
   setup() {
     const articles = ref<Article[]>([]);
-    const selectedArticle = ref<number | null>(null);
+    const selectedArticle = ref<number | undefined>(undefined);
 
     const newArticle = ref<{
       name: string;
@@ -210,13 +84,9 @@ export default defineComponent({
       tags: [],
     });
 
-    const tagInput = ref("");
-    const editTagInput = ref("");
-
-    function addTag() {
-      if (tagInput.value.trim() !== "") {
-        newArticle.value.tags.push(tagInput.value.trim());
-        tagInput.value = "";
+    function addTag(tagInput: string) {
+      if (tagInput.trim() !== "") {
+        newArticle.value.tags.push(tagInput.trim());
       }
     }
 
@@ -224,10 +94,10 @@ export default defineComponent({
       newArticle.value.tags.splice(index, 1);
     }
 
-    function addEditTag() {
-      if (editTagInput.value.trim() !== "") {
-        editedArticle.value.tags.push(editTagInput.value.trim());
-        editTagInput.value = "";
+    function addEditTag(editTagInput: string) {
+      if (editTagInput.trim() !== "") {
+        editedArticle.value.tags.push(editTagInput.trim());
+        editTagInput = "";
       }
     }
 
@@ -246,12 +116,12 @@ export default defineComponent({
 
     async function createArticle() {
       try {
-        const response = await axios.post<Article>(
+        await axios.post<Article>(
           "http://localhost:3001/articles",
           newArticle.value
         );
-        articles.value.push(response.data);
         newArticle.value = { name: "", description: "", tags: [] };
+        await fetchArticles();
       } catch (error) {
         console.error(error);
       }
@@ -262,6 +132,8 @@ export default defineComponent({
       editedArticle.value = {
         ...articles.value[index],
       };
+      console.log(editedArticle.value);
+      console.log(articles.value[index]);
     }
 
     async function updateArticle() {
@@ -272,11 +144,22 @@ export default defineComponent({
           editedArticle.value
         );
         articles.value[selectedArticle.value!] = response.data;
-        selectedArticle.value = null;
+        selectedArticle.value = undefined;
         const response2 = await axios.get<Article[]>(
           "http://localhost:3001/articles"
         );
         articles.value = response2.data;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+
+    async function deleteArticle() {
+      try {
+        const id = articles.value[selectedArticle.value!].id;
+        await axios.delete<Article>(`http://localhost:3001/articles/${id}`);
+        articles.value.splice(selectedArticle.value!, 1);
+        selectedArticle.value = undefined;
       } catch (error) {
         console.error(error);
       }
@@ -290,8 +173,7 @@ export default defineComponent({
       createArticle,
       selectArticle,
       updateArticle,
-      tagInput,
-      editTagInput,
+      deleteArticle,
       addTag,
       removeTag,
       addEditTag,
