ed9f4a2b026776e377aa11fd94331211712be768
mikhael dantas <mikhael-dcs@outlook.com>
2023-04-02 19:27:41 -0300

    example pages

---
+++ src/views/HelloWorld2.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-blue-500 p-4">
+    <h1>Hello World 2</h1>
+    <p>Content for Hello World 2 page goes here...</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HelloWorld2",
+});
+</script>
---
+++ src/views/HelloWorld3.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="bg-yellow-500 p-4">
+    <h1>Hello World 3</h1>
+    <p>Content for Hello World 3 page goes here...</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "HelloWorld3",
+});
+</script>
